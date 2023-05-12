c6a9164ade282d4ff30cce3b0fdaf95e4b1e8801
quangfoden <123524896+quangfoden@users.noreply.github.com>
2023-05-12 11:51:10 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const root = document.querySelector(':root');
-
 //SHOW HIDE RIGHT 
 
 // popup 
@@ -9,6 +8,52 @@ const changeActiveItem = () => {
         item.classList.remove('active');
     })
 }
+
+// click icon show hide sidebar 
+const clickShowsidebar = document.querySelector('.icon-show-sidebar');
+clickShowsidebar.onclick = () => {
+    //console.log('click')
+    let siDebar = document.querySelector('.sidebar');
+    siDebar.style.display = 'block';
+}
+const clickHidesidebar = document.querySelector('.icon-close-sidebar')
+clickHidesidebar.onclick = () => {
+    // console.log('click')
+    let siDebar = document.querySelector('.sidebar');
+    siDebar.style.display = 'none';
+}
+//========CLICK WINDOW SHOW HIDE
+let showMessages = document.querySelector('.messages');
+let showNotifications = document.querySelector('.notification-popup');
+let showRequests = document.querySelector('.friend-requests')
+document.addEventListener("mousedown", function (event) {
+    // Nếu người dùng không nhấp chuột vào phần tử
+    if (!showMessages.contains(event.target)) {
+        // Ẩn phần tử
+        showMessages.style.display = "none";
+    }
+
+    if (!showNotifications.contains(event.target)) {
+        // Ẩn phần tử
+        showNotifications.style.display = "none";
+    }
+
+    if (!showRequests.contains(event.target)) {
+        // Ẩn phần tử
+        showRequests.style.display = "none";
+    }
+});
+// let showMessages = document.querySelector('.messages');
+// let showNotifications = document.querySelector('.notification-popup');
+// let showRequests = document.querySelector('#new-myfriend')
+// window.onclick = () => {
+//     if (showNotifications.style.display === 'block') {
+//         showNotifications.style.display = 'none'; //}
+//         // showMessages.style.display = 'none'; //
+//         // showRequests.style.display = 'none'; //
+//     }
+// }
+
 
 menuItems.forEach(item => {
     let notification = document.querySelector('.notification-popup')
@@ -24,6 +69,8 @@ menuItems.forEach(item => {
         }
     })
 })
+
+
 // ===== 
 
 
@@ -36,20 +83,45 @@ const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
 // searcher chat 
 const searchMessages = () => {
-    const val = messageSearch.value.toLowerCase();
+    //const val = messageSearch.value.toLowerCase();
     // console.log(val);
+    const val = messageSearch.value;
     message.forEach(user => {
         let name = user.querySelectorAll('h5')
-        name.textContent.toLowerCase();
-        if (name.indexOf(val) != -1) {
-            user.style.display = 'flex';
-        } else {
-            user.style.display = 'none';
+        // name.textContent.toLowerCase();
+        // if (name.indexOf(val) != -1) {
+        //     user.style.display = 'flex';
+        // } else {
+        //     user.style.display = 'none';
+        // }
+        for (let i = 0; i < message.length; i++) {
+            let nameText = message[i].textContent.toLowerCase();
+            if (nameText.indexOf(val.toLowerCase()) != -1) {
+                message[i].style.display = 'flex';
+            } else {
+                message[i].style.display = 'none';
+            }
         }
+
     })
 }
 //search chat 
 messageSearch.addEventListener('keyup', searchMessages);
+// =====
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id !== 'messages-notifications') {
+            messages.style.display = 'none';
+        }
+        else {
+            messages.style.display = 'block';
+        }
+    })
+})
+
+
 
 // hightlight 
 messageNotifition.addEventListener('click', () => {
@@ -57,7 +129,6 @@ messageNotifition.addEventListener('click', () => {
     messageNotifition.querySelector('.notification-count').style.display = 'none';
     messages.style.boxShadow = '0 0 1rem var(--color-primary)'
     setTimeout(() => { messages.style.boxShadow = 'none' }, 2000);
-    messages.classList.toggle('showmessages');
 })
 
 // ==========END MESSAGE==============
@@ -70,8 +141,21 @@ requests.addEventListener('click', () => {
     requests.querySelector('.notification-count').style.display = 'none';
     request.style.boxShadow = '0 0 1rem var(--color-primary)'
     setTimeout(() => { request.style.boxShadow = 'none' }, 3000);
-    request.classList.toggle('showrequests');
 });
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id !== 'new-myfriend') {
+            request.style.display = 'none';
+        }
+        else {
+            request.style.display = 'block';
+        }
+    })
+})
+
+
 //Theme color /customsize
 
 const theme = document.querySelector('#theme');
@@ -100,7 +184,7 @@ const removeSizeselector = () => {
     })
 }
 
-
+// ====================================================
 fontSizes.forEach(size => {
     size.addEventListener('click', (e) => {
         //console.log('chosen size')
